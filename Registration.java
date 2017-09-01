8b7ded237e6e42c95d7f01b3e3250b86f1e5fe46
prakash1997raj <31536826+prakash1997raj@users.noreply.github.com>
2017-09-01 17:01:42 +0530

    Add files via upload

---
+++ Registration.java
@@ -0,0 +1,14 @@
+package com.example.sampleprakash;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Registration extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.reg);
+	}
+
+}
